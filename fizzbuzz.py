dfb394a5b26ab91ae3ec08e61ddf299fd25c706e
d-yeon <d.yeon819@gmail.com>
2023-11-17 12:21:11 +0900

    feat: Print fizz if i is multiple of 3

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,4 +2,10 @@
 ## vi fizzbuzz.py
 ### i -> print('hello') -> esc -> :wq + enter
 
-print('hello')
+# branch 이동 시에는 무조건 commit 을 하고 넘어갈 것 !
+
+for i in range(1,10+1):
+    if i%3==0:
+        print('fizz')
+    else:
+        print(i)
